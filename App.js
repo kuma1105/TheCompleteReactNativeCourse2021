46acc474b898ecda8c59f36cab1c75515f819c5b
Fancyee <74527909+kuma1105@users.noreply.github.com>
2022-07-09 13:53:37 +0900

    Use text inpu & keyboard

--- App.js
+++ App.js
@@ -1,121 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, ScrollView, RefreshControl, FlatList, SectionList } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView, RefreshControl, FlatList, SectionList, TextInput } from 'react-native';
 
 import { useState } from 'react';
 
 export default function App() {
-  // const [name, setName] = useState('Style Test')
 
-  // const onClickHandler = () => {
-  //   setName('Programming with Mash')
-  // }
-
-  // const [session, setSession] = useState({ number: 6, title: 'state' })
-  // const [current, setCurrent] = useState(true)
-
-  // const onClickHandler = () => {
-  //   setName('Programming With Mash')
-  //   setSession({ number: 7, title: 'Style' })
-  //   setCurrent(false)
-  // }
-
-  const [Items, setItems] = useState([
-    { name: 'Item 1' },
-    { name: 'Item 2' },
-    { name: 'Item 3' },
-    { name: 'Item 4' },
-    { name: 'Item 5' },
-    { name: 'Item 6' },
-    { name: 'Item 7' },
-    { name: 'Item 8' },
-  ])
-
-  const DATA = [
-    {
-      title: 'Title 1',
-      data: ['Item 1-1', 'Item 1-2', 'Item 1-3']
-    },
-    {
-      title: 'Title2-1',
-      data: ['Item 2-1', 'Item 2-2', 'Item 2-3']
-    },
-    {
-      title: 'Title 3',
-      data: ['Item 3-1']
-    },
-    {
-      title: 'Title 4',
-      data: ['Item 4-1', 'Item 4-2']
-    },
-  ]
-
-  const [Refreshing, setRefreshing] = useState(false);
-  const onRefresh = () => {
-    setRefreshing(true);
-    setItems([...Items, { key: 33, name: 'item 33' }]);
-    setRefreshing(false);
-  }
+  const [name, setName] = useState('');
 
   return (
-    <SectionList
-      keyExtractor={(item, index) => index.toString()}
-      sections={DATA}
-      renderItem={({ item }) => (
-        <Text style={styles.text}>{item}</Text>
-      )}
-      renderSectionHeader={({ section }) => (
-        <View style={styles.item}>
-          <Text style={styles.text}>{section.title}</Text>
-        </View>
-      )}
-    />
-
-    // <FlatList
-    //   // horizontal
-    //   // inverted
-    //   keyExtractor={(item, index) => index.toString()}
-    //   data={Items}
-    //   renderItem={({ item }) => (
-    //     <View style={styles.item}>
-    //       <Text style={styles.text}>{item.name}</Text>
-    //     </View>
-    //   )}
-    //   refreshControl={<RefreshControl refreshing={Refreshing} onRefresh={onRefresh} colors={['#ff00ff']} />}
-    // />
-
-    // <ScrollView horizontal={false} style={styles.body}
-    //   refreshControl={<RefreshControl refreshing={Refreshing} onRefresh={onRefresh} colors={['#ff00ff']} />}
-    // >
-    //   {
-    //     Items.map((object) => {
-    //       return (
-    //         <View style={styles.item} key={object.key}>
-    //           <Text style={styles.text}>{object.item}</Text>
-    //         </View>
-    //       )
-    //     })
-    //   }
-    // </ScrollView>
+    <View style={styles.body}>
+      <Text style={styles.text}>
+        Please write your name:
+      </Text>
+      <TextInput style={styles.input} placeholder='HEllO WORLD'
+        onChangeText={(value) => setName(value)}
+        multiline
+        keyboardType='numeric'
+        maxLength={4}
+      />
+      <Text style={styles.text}>
+        Your name is: {name}
+      </Text>
+    </View>
   );
+
 }
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    flexDirection: 'column',
     backgroundColor: '#ffffff',
-  },
-  item: {
-    margin: 10,
-    backgroundColor: '#4ae1fa',
-    justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   text: {
     color: '#000000',
-    fontSize: 45,
-    fontStyle: 'italic',
+    fontSize: 20,
+    margin: 10
   },
+  input: {
+    width: 200,
+    borderWidth: 1,
+    borderColor: '#555',
+    borderRadius: 5,
+    textAlign: 'center',
+    fontSize: 15,
+  }
   // view1: {
   //   // width: 100,
   //   // height: 100,
